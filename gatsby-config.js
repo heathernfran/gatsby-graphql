4eca685ff0730e0321ede4868da4d0cc876fcb87
Heather Frantisak <hfrantisak@mvnp.com>
2017-09-20 14:57:04 -1000

    Configure typography, using bootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js',
+      }
+    },
     /*
      * Gatsby's data processing layer begins with “source”
      * plugins. Here the site sources its data from Wordpress.
