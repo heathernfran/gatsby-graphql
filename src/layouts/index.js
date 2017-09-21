1f3853fc8ff6eb160947cce87a9ba94711f48916
Heather Frantisak <hfrantisak@mvnp.com>
2017-09-20 15:33:21 -1000

    Remove boilerplate for header, template layout. Use container component for layout.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,35 +3,14 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+import Container from '../components/container'
+
 import './index.css'
 
 const Header = () => (
   <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+    style={{}}
+  ></div>
 )
 
 const TemplateWrapper = ({ children }) => (
@@ -44,16 +23,9 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <Container>
       {children()}
-    </div>
+    </Container>
   </div>
 )
 
