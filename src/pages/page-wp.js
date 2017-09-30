4e71f1c79cb97b78a37e85693e5276472e10ba5b
Heather Frantisak <hfrantisak@mvnp.com>
2017-09-29 16:44:02 -1000

    Create component for links in a list, with inline styles

--- src/pages/page-wp.js
+++ src/pages/page-wp.js
@@ -2,17 +2,26 @@ import React from 'react'
 import Link from 'gatsby-link'
 import CreateMarkup from '../helpers/CreateMarkup'
 
+const ListLink = props =>
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+
 export default ({ data }) => (
   <div>
-    <Link to="/">Go back to the homepage</Link>
     {data.allWordpressPage.edges.map(({ node }) =>
       <div
         key={node.id}
         dangerouslySetInnerHTML={CreateMarkup(node.title)} 
       />
     )}
-    <Link to="/blog/">Go to blog</Link>
-    <Link to="/gallery/">Go to gallery</Link>
+    <ul style={{ listStyle: `none` }}>
+      <ListLink to="/">Go back to the homepage</ListLink>
+      <ListLink to="/blog/">Go to blog</ListLink>
+      <ListLink to="/gallery/">Go to gallery</ListLink>
+    </ul>
   </div>
 )
 
